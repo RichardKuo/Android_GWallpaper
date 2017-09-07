90fd2878533d4a111a859dac45471d67db6b6d8f
RichardKuo <guoyaohong1989@163.com>
2017-09-07 12:42:52 +0800

    add more pictures loop show function

--- app/src/main/java/org/gyh/wallpaper/SimpleWallpaper.java
+++ app/src/main/java/org/gyh/wallpaper/SimpleWallpaper.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.os.Handler;
+import android.os.Message;
 import android.service.wallpaper.WallpaperService;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -24,19 +25,59 @@ import android.view.WindowManager;
 public class SimpleWallpaper extends WallpaperService {
     private static final String TAG = SimpleWallpaper.class.getSimpleName();
 
-    private SimpleEngine mSimpleEngine = null;
+    private static final int MSG_RELOAD = 1;
+
+
     private WindowManager mWindowManager = null;
+    private SimpleEngine mSimpleEngine = null;
+    private int[] mDefaultWallpaperIds = new int[]{
+            R.drawable.wallpaper_default_1,
+            R.drawable.wallpaper_default_2,
+            R.drawable.wallpaper_default_3};
+    private int mIndexOfWallpaper = 0;
     private int mWidth = 0;
     private int mHeight = 0;
+    private Object mIndexLock = new Object();
+    private boolean mRunning = false;
+    private Bitmap mBitmap = null;
+    private int mBitmapWidth = 0;
+    private int mBitmapHeight = 0;
+
+
+    private Handler mUIH = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+
+        }
+    };
+
+    private Handler mIndexUpdateHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            Log.i(TAG, "handleMessage(), mRunning = " + mRunning);
+            if(mRunning) {
+                if (mIndexOfWallpaper == mDefaultWallpaperIds.length - 1) {
+                    mIndexOfWallpaper = 0;
+                } else {
+                    mIndexOfWallpaper++;
+                }
+                loadBitmap();
+                mSimpleEngine.draw();
+            }
+
+            mIndexUpdateHandler.sendEmptyMessageDelayed(0, 10 * 1000);
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         Log.i(TAG, "onCreate()");
-        if(null == mSimpleEngine) {
-            mSimpleEngine = new SimpleEngine();
-        }
 
         mWindowManager = (WindowManager)this.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics dm = new DisplayMetrics();
@@ -44,6 +85,8 @@ public class SimpleWallpaper extends WallpaperService {
         mWidth = dm.widthPixels;
         mHeight = dm.heightPixels;
         Log.i(TAG, "onCreate()::mWidth = " + mWidth + ", mHeight = " + mHeight);
+
+        mIndexUpdateHandler.sendEmptyMessage(0);
     }
 
     @Override
@@ -56,10 +99,8 @@ public class SimpleWallpaper extends WallpaperService {
     @Override
     public Engine onCreateEngine() {
         Log.i(TAG, "onCreateEngine()");
-
-        if(null == mSimpleEngine) {
-            mSimpleEngine = new SimpleEngine();
-        }
+        // 这里需要再考虑优化下，每次call onCreateEngine()都直接创建一个SimpleEngine的做法有点不妥
+        mSimpleEngine = new SimpleEngine();
         return mSimpleEngine;
     }
 
@@ -70,13 +111,42 @@ public class SimpleWallpaper extends WallpaperService {
         Log.i(TAG, "onConfigurationChanged()");
     }
 
+    private void loadBitmap() {
+        BitmapFactory.Options option = new BitmapFactory.Options();
+        option.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), mDefaultWallpaperIds[mIndexOfWallpaper], option);
+        Log.i(TAG, "loadBitmap(), option.outWidth = " + option.outWidth + ", option.outHeight = " + option.outHeight);
+        option.inJustDecodeBounds = false;
+        option.inSampleSize = option.outWidth / mWidth;
+        Log.i(TAG, "loadBitmap(), mIndexOfWallpaper = " + mIndexOfWallpaper);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), mDefaultWallpaperIds[mIndexOfWallpaper], option);
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Matrix matrix = new Matrix();
+        // 这里必须将参数转换为float值之后再做除法计算，否则得出来的scale数据均为0.0，
+        // 会导致后面的Bitmap.createBitmap()的参数异常
+        float scaleWidth = (float)mWidth / width;
+        float scaleHeight = (float)mHeight / height;
+        matrix.postScale(scaleWidth, scaleHeight);
+        Log.i(TAG, "loadBitmap(), width = " + width + ", height = " + height + ", scaleWidth = " + scaleWidth + ", scaleHeight = " + scaleHeight);
+        if(null != mBitmap) {
+            synchronized (mBitmap) {
+                mBitmap.recycle();
+                mBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+            }
+        } else {
+            mBitmap= Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+        }
+        bitmap.recycle();
+        mBitmapWidth = mBitmap.getWidth();
+        mBitmapHeight = mBitmap.getHeight();
+
+        Log.i(TAG, "loadBitmap(), mBitmapWidth = " + mBitmapWidth + ", mBitmapHeight = " + mBitmapHeight);
+    }
+
+
     class SimpleEngine extends WallpaperService.Engine {
         private final String TAG = SimpleEngine.class.getSimpleName();
-
-        private Handler mH = new Handler();
-        private Bitmap mBitmap = null;
-        private int mBitmapWidth = 0;
-        private int mBitmapHeight = 0;
 
         @Override
         public boolean isVisible() {
@@ -98,28 +168,7 @@ public class SimpleWallpaper extends WallpaperService {
             super.onCreate(surfaceHolder);
 
             Log.i(TAG, "onCreate()");
-            BitmapFactory.Options option = new BitmapFactory.Options();
-            option.inJustDecodeBounds = true;
-            BitmapFactory.decodeResource(getResources(), R.drawable.wallpaper_default, option);
-            Log.i(TAG, "onCreate(), option.outWidth = " + option.outWidth + ", option.outHeight = " + option.outHeight);
-            option.inJustDecodeBounds = false;
-            option.inSampleSize = option.outWidth / mWidth;
-            mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.wallpaper_default, option);
-            int width = mBitmap.getWidth();
-            int height = mBitmap.getHeight();
-            Matrix matrix = new Matrix();
-            // 这里必须将参数转换为float值之后再做除法计算，否则得出来的scale数据均为0.0，
-            // 会导致后面的Bitmap.createBitmap()的参数异常
-            float scaleWidth = (float)mWidth / width;
-            float scaleHeight = (float)mHeight / height;
-            matrix.postScale(scaleWidth, scaleHeight);
-            Log.i(TAG, "onCreate(), width = " + width + ", height = " + height + ", scaleWidth = " + scaleWidth + ", scaleHeight = " + scaleHeight);
-            Bitmap bitmap = Bitmap.createBitmap(mBitmap, 0, 0, width, height, matrix, true);
-            mBitmap.recycle();
-            mBitmap = bitmap;
-            mBitmapWidth = mBitmap.getWidth();
-            mBitmapHeight = mBitmap.getHeight();
-            Log.i(TAG, "onCreate(), mBitmapWidth = " + mBitmapWidth + ", mBitmapHeight = " + mBitmapHeight);
+            loadBitmap();
         }
 
         @Override
@@ -127,6 +176,9 @@ public class SimpleWallpaper extends WallpaperService {
             super.onDestroy();
 
             Log.i(TAG, "onDestroy()");
+            if(null != mBitmap) {
+                mBitmap.recycle();
+            }
         }
 
         @Override
@@ -135,8 +187,10 @@ public class SimpleWallpaper extends WallpaperService {
 
             Log.i(TAG, "onVisibilityChanged(), visible = " + visible);
             if(visible) {
+                mRunning = true;
                 draw();
             } else {
+                mRunning = false;
                 clear();
             }
         }
@@ -188,20 +242,26 @@ public class SimpleWallpaper extends WallpaperService {
         private void draw() {
             Log.i(TAG, "draw()");
             Canvas canvas = getSurfaceHolder().lockCanvas();
-            Paint paint = new Paint();
-            // clear screen
-            canvas.drawColor(Color.GRAY);
-            canvas.drawBitmap(mBitmap, 0, 0, paint);
-            getSurfaceHolder().unlockCanvasAndPost(canvas);
+            if(null != canvas) {
+                synchronized (mBitmap) {
+                    Paint paint = new Paint();
+                    // clear screen
+                    canvas.drawColor(Color.GRAY);
+                    canvas.drawBitmap(mBitmap, 0, 0, paint);
+                    getSurfaceHolder().unlockCanvasAndPost(canvas);
+                }
+            }
         }
 
         private void clear() {
             Log.i(TAG, "clear()");
             Canvas canvas = getSurfaceHolder().lockCanvas();
-            Paint paint = new Paint();
-            // clear screen
-            canvas.drawColor(Color.GRAY);
-            getSurfaceHolder().unlockCanvasAndPost(canvas);
+            if(null != canvas) {
+                Paint paint = new Paint();
+                // clear screen
+                canvas.drawColor(Color.GRAY);
+                getSurfaceHolder().unlockCanvasAndPost(canvas);
+            }
         }
     }
 }
